6811e78e94c734377d4df10924c7267d8179404b
Gokulanandan <gokulanandans1993@outlook.com>
2024-05-19 09:40:31 +0530

    First Commit

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,4 +1,34 @@
 package Sorting_Algorithms;
 
+import java.util.Arrays;
+
 public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {6,4,5,8,1};
+        selection_Sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+     static int findLargest(int[] arr,int start,int end){
+        int max = start;
+        for(int i=start;i<=end;i++){
+            if(arr[i] > arr[max]) {
+                max = i;
+            }
+        }
+        return max;
+    }
+    static void selection_Sort(int[] arr){
+        for(int i=0;i<arr.length;i++) {
+            int last = arr.length -1 -i;
+            int max = findLargest(arr,0,last);
+            if(max != last) {
+                int temp = arr[last];
+                arr[last] = arr[max];
+                arr[max] = temp;
+            }
+        }
+    }
 }
+
+
